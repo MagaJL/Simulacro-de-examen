5ffb6f044aa3c4d504e82870547f8bc51247118f
MagaJL <48716048@est.ort.edu.ar>
2025-04-10 10:42:52 -0300

    Intento de heladeria

--- Assets/Scripts/Respuesta.cs
+++ Assets/Scripts/Respuesta.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public class Respuesta : MonoBehaviour
 {
+    public string codigoGusto;
+    public int cantHelado;
+
+    float precioPorKilo = 500;
+    float precioTotal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (cantHelado < 250 || cantHelado > 300)
+        {
+            Debug.Log("Cantidad de helado no válida.");
+            return;
+        }
+        if (codigoGusto != "CHO" && codigoGusto != "DDL" && codigoGusto != "FRU")
+        {
+            Debug.Log("Código de helado no válido");
+            return;
+        }
+
+        precioTotal = cantHelado * (precioPorKilo / 1000);
+
+        if (codigoGusto == "FRU")
+        {
+            precioTotal = precioTotal * 0.1f;
+        }
+
+        Debug.Log("Debe pagar: " + precioTotal);
+
     }
 
     // Update is called once per frame
